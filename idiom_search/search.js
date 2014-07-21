8f90941b4da11d8cc4c47e282d22c2fa727a89d9
ahsyuyu <ahsyuyu@gmail.com>
2014-07-21 13:56:35 +0800

    update idioms_search

--- idiom_search/search.js
+++ idiom_search/search.js
@@ -11,7 +11,22 @@ var search=function(tofind){
 			//out.replace(searchword_re, changecolor(searchword) );
 		}
 	}
+	return out;
+}
 
+var search_def=function(tofind){
+	var out=[];
+	//var searchword="tofind";
+	var searchword=new RegExp(tofind,"g");
+
+	for(var i=0;i<idioms.length;i++){
+		var find=idioms[i].def.match(searchword);
+		if(find){
+			idioms[i].def=idioms[i].def.replace(searchword,changecolor);
+			out.push(idioms[i]);
+			//out.replace(searchword_re, changecolor(searchword) );
+		}
+	}
 	return out;
 }
 
--- idiom_search/ui.js
+++ idiom_search/ui.js
@@ -6,4 +6,12 @@ var dosearch=function(tofind){
 	//var tofind=document.getElementById("tofind").value;
 	var arr=search(tofind);
 	document.getElementById("result").innerHTML=arr.map(showidiom).join("<br>");
+	document.getElementById("result_num").innerHTML=arr.length+" results";
+}
+
+var dosearch_def=function(tofind){
+	//var tofind=document.getElementById("tofind").value;
+	var arr=search_def(tofind);
+	document.getElementById("result").innerHTML=arr.map(showidiom).join("<br>");
+	document.getElementById("result_num").innerHTML=arr.length+" results";
 }
