bc3cd4fcfabb8d0a3f07906eb1a8c386307aec1f
ahsyuyu <ahsyuyu@gmail.com>
2014-07-23 10:14:44 +0800

    undefine problem

--- idiom_search/ui.js
+++ idiom_search/ui.js
@@ -11,7 +11,7 @@ var dosearch=function(tofind){
 
 var dosearch_def=function(tofind){
 	//var tofind=document.getElementById("tofind").value;
-	var arr=search_def(tofind);
+	var arr=search(tofind,def);
 	document.getElementById("result").innerHTML=arr.map(showidiom).join("<br>");
 	document.getElementById("result_num").innerHTML=arr.length+" results";
 }
