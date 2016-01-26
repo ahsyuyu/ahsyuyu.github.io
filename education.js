d1e5c3d18ac5c5643743c41c83ec766efc1bf65c
ahsyuyu <ahsyuyu@gmail.com>
2016-01-26 13:44:32 -0800

    fix animation not working in firefox and add new content

--- education.js
+++ education.js
@@ -6,3 +6,21 @@ $(document).ready(function (){
         }, 1000);
     });
 });
+
+$(document).ready(function (){
+    $(".top").click(function (item){
+    	var sec = item.currentTarget.id;
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000);
+    });
+});
+
+$(document).ready(function (){
+    $(".elist").click(function (item){
+        var sec = item.currentTarget.id;
+        $('html, body').animate({
+            scrollTop: $("div[sec='" + sec + "']" ).offset().top - 20
+        }, 1000);
+    });
+});
